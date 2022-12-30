2a84887a3b5195dd9110df2307fcdc4e6622c13e
Arnie <arnie752@gmail.com>
2022-12-30 11:56:22 +0300

    Created Eloquent Models

---
+++ database/factories/RegisteredUnitsFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RegisteredUnits>
+ */
+class RegisteredUnitsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
